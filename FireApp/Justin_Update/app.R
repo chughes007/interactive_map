a0d806b0883d3badca4191ba5dca2c70231e94a3
Justin Heyerdahl <36862768+jheyerdahl@users.noreply.github.com>
2018-03-12 11:43:17 -0700

    Add files via upload

--- FireApp/Justin_Update/app.R
+++ FireApp/Justin_Update/app.R
@@ -8,11 +8,6 @@
 #
 
 
-# Lives as lab_5 app
-
-
-
-
 # Final App, dashboard
 
 
@@ -24,31 +19,12 @@ library(sp)
 library(raster)
 library(tidyverse)
 
-Subset_2000 <- read.csv("Subset_2000.csv")
-
-SDI260_CFL_Change <- Subset_2000 %>% 
-  mutate(CFL_Change = Average_260 - Average_nt) %>% 
-  select(CFL_Change)
-
-
-choice <- c("standras", "aspectras30", "SlopTeal30d")
-
-standras <- raster("standras.tif")
-
-aspectras30 <- raster("aspectras30.tif")
-
-tiff_stack <- raster::stack("standras.tif", "aspectras30.tif", "SlopTeal30d.tif")
-
 private <- st_read(dsn = ".", layer = "Private_Parcels")
 
 private_t <- st_transform(private, "+init=epsg:4326")
 
 private_tclass <- private_t %>% 
   select(Stand_Id)
-
-
-pal <- colorNumeric(c("#0C2C84", "#41B6C4", "#FFFFCC"), values(tiff_stack),
-                    na.color = "transparent")
 
 
 veg_types <- st_read(dsn = ".", layer = "RegAgg")
@@ -68,77 +44,31 @@ regime_df <- st_transform(fireregime, "+init=epsg:4326")
 regime_class <- regime_df %>%
   select(FireRegime)
 
-pines <- veg_df %>%
-  filter(Type == "Pines")
-oaks <- veg_df %>%
-  filter(Type == "Oaks")
-shrubs <- veg_df %>%
-  filter(Type == "Shrubs")
-wet <- veg_df %>%
-  filter(Type == "Wet")
-firs <- veg_df %>%
-  filter(Type == "Firs")
-grass <- veg_df %>%
-  filter(Type == "Grasses_and_Forbs")
-other <- veg_df %>%
-  filter(Type == "Other")
-resistant <- veg_df %>%
-  filter(Fire == "Resistant")
-not_resistant <- veg_df %>%
-  filter(Fire == "Not_Resistant")
-neutral <- veg_df %>%
-  filter(Fire == "Neutral")
-
+Low_Severity <- regime_class %>%
+  filter(FireRegime == "I - Low Severity Fire")
+Mid_Severity <- regime_class %>%
+  filter(FireRegime == "III - Mixed Severity FIre")
+High_Severity <- regime_class %>%
+  filter(FireRegime == "IV = High Severity Fire")
 
 SAF_Cover <- st_read(dsn = ".", layer = "ForestCover")
 SAF_Cover_df <- st_transform(SAF_Cover, "+init=epsg:4326")
 SAF_class <- SAF_Cover_df %>%
   select(SAF_Name)
 
-Mountain_Hemlock <- SAF_Cover_df %>%
-  filter(SAF_Name == "Mountain Hemlock")
-Red_Fir <- SAF_Cover_df %>%
-  filter(SAF_Name == "Red Fir")
-Whitebark_Pine <- SAF_Cover_df %>%
-  filter(SAF_Name == "Whitebark Pine")
-White_Fir <- SAF_Cover_df %>%
-  filter(SAF_Name == "White Fir")
-Western_White_Pine <- SAF_Cover_df %>%
-  filter(SAF_Name == "Western White Pine")
-Aspen <- SAF_Cover_df %>%
-  filter(SAF_Name == "Aspen")
-Lodgepole_Pine <- SAF_Cover_df %>%
-  filter(SAF_Name == "Lodgepole Pine")
-Cottonwood_Willow <- SAF_Cover_df %>%
-  filter(SAF_Name == "Cottonwood - Willow")
-Mixed_Conifer <- SAF_Cover_df %>%
-  filter(SAF_Name == "Sierra Nevada Mixed Conifer")
-Ponderosa_Pine <- SAF_Cover_df %>%
-  filter(SAF_Name == "Pacific Ponderosa Pine")
-Black_Oak <- SAF_Cover_df %>%
-  filter(SAF_Name == "California Black Oak")
-Jeffrey_Pine <- SAF_Cover_df %>%
-  filter(SAF_Name == "Jeffrey Pine")
-Canyon_Live_Oak <- SAF_Cover_df %>%
-  filter(SAF_Name == "Canyon Live Oak")
-Blue_Oak <- SAF_Cover_df %>%
-  filter(SAF_Name == "Blue Oak - Gray Pine")
-Cost_Live_Oak <- SAF_Cover_df %>%
-  filter(SAF_Name == "California Coast Live Oak")
-Mixed_Subalpine <- SAF_Cover_df %>%
-  filter(SAF_Name == "California Mixed Subalpine")
-Hard_Chaparral <- SAF_Cover_df %>%
-  filter(SAF_Name == "Hard Chaparral")
-
 dinkey_boundary <- st_read(dsn = ".", layer = "DinkeyBoundary")
 dinkey_df <- st_transform(dinkey_boundary, "+init=epsg:4326")
 
-
+SAFNames <- unique(SAF_class$SAF_Name)
 palrainbow <- colorFactor(palette = rainbow(18), domain = SAFNames)
+
+RegNames <- unique(veg_name$Name)
 palrainbow_regdom <- colorFactor(palette = rainbow(51), domain = RegNames)
 
-SAFNames <- unique(SAF_class$SAF_Name)
-RegNames <- unique(veg_name$Name)
+RegimeNames <- unique(fireregime$FireRegime)
+palfireregime <- colorFactor(c("brown", "yellow","orange","red", "grey", "lightblue"), RegimeNames[1:6])
+
+
 # Adapting this for my data
 
 #requires: SDI260_CFL_Change from SavingSierras_Stats.Rmd (which requires Subset_2000.csv)
@@ -155,11 +85,8 @@ ui <- dashboardPage(
     
     sidebarMenu(
       
-      menuItem("First Tab", tabName = "tab_1"),
-      menuItem("Second Tab", tabName = "tab_2"),
-      menuItem("Historic Fire", tabName = "tab_3"),
-      menuItem("Fuel Loading", tabName = "tab_4"),
-      menuItem("Forest Cover", tabName = "tab_5")
+      menuItem("Fire History", tabName = "tab_3"),
+      menuItem("Forest Cover", tabName = "tab_4")
   
       )
       
@@ -168,106 +95,53 @@ ui <- dashboardPage(
   
   dashboardBody(
     
-    
-    tabItems(
-      
-      tabItem(tabName = "tab_1",
-              fluidRow(
-                box(plotOutput("my_graph1", height = 342)), 
-                box(title = "Choose Color:", 
-                    sliderInput("bins",
-                                "Number of bins:",
-                                min = 1,
-                                max = 50,
-                                value = 30))
-              )),
-      
-      tabItem(tabName = "tab_2",
-              fluidRow(
-                box(leafletOutput("my_graph2", height = 432)),
-                box(title = "Choose Map:",
-                    selectInput("class", 
-                                "Choose Map:", 
-                                choices = choice))
-              )),
-      
+
       tabItem(tabName = "tab_3",
               fluidRow(
                 box(leafletOutput("my_graph3", height = 432)),
-                box(title = "Departure from Historical Fire Regime",
-                    selectInput("condclass", 
-                                "Choose Departure Level:", 
-                                choices = unique(cond_class$Departure)))
+                box(title = "Historical Fire Regime",
+                    selectInput("regime_class", 
+                                "Choose Regime Level:", 
+                                choices = unique(regime_class$FireRegime)))
                 )
               ),
       
       tabItem(tabName = "tab_4",
               fluidRow(
-                box(leafletOutput("my_graph4", height = 432)),
-                box(title = "Regional Dominant Vegetation")
-              )
-      ),
-      
-      tabItem(tabName = "tab_5",
-              fluidRow(
-                box(leafletOutput("my_graph5", height = 700, width = 700))
+                box(leafletOutput("my_graph4", height = 700, width = 700))
               )
       
               )
       )
     )
-  )
   
 
     
   
 server <- function(input, output){
   
-  output$my_graph1 <- renderPlot({
-    # generate bins based on input$bins from ui.R
-    x    <- SDI260_CFL_Change$CFL_Change 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-  })
-  
-  output$my_graph2 <- renderLeaflet({
-    
-    tiffmap <- subset(tiff_stack, input$class, drop=TRUE)
-
-    leaflet(private_tclass) %>% 
-      addTiles() %>% 
-      addRasterImage(tiffmap, colors = pal, opacity = 0.8) %>% 
-      addLegend (pal = pal, values = values(tiffmap),
-                 title = "This is Map") %>% 
-      addPolygons(color = "black",
-                  weight = 0.5)})
-  
   
   output$my_graph3 <- renderLeaflet({
-    cond_sub <- cond_class %>%
-      filter(Departure == input$condclass)
+    regime_sub <- regime_class %>%
+      filter(FireRegime == input$regime_class)
 
-    leaflet(cond_sub) %>% 
+    leaflet(regime_sub) %>% 
       addTiles() %>% 
-      addPolygons(data = cond_class,
-                  weight = 0.5,
-                  color = "red",
-                  fillColor = "red",
+      addPolygons(weight = 0.5,
+                  color = "black",
+                  fillColor = ~palfireregime(RegimeNames),
                   fillOpacity = 0.5) %>%
       addPolygons(data = dinkey_df,
                   weight = 1,
                   color = "black",
-                  fillColor = "grey",
-                  fillOpacity = 0.3) %>%
+                  fillColor = "transparent") %>%
       addPolygons(data = private_tclass,
                   weight = 0.5,
                   color = "black",
                   fillColor = "yellow",
                   fillOpacity = 0.3)})
     
-  output$my_graph5 <- renderLeaflet({
+  output$my_graph4 <- renderLeaflet({
     leaflet() %>% 
       addTiles() %>% 
       addPolygons(data = private_tclass,
