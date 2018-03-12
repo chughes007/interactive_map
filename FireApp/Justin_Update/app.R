5c5e59899d68f9c90b57f545fae8910b93ad0a10
ChrisHughes <cjhughes@ucsb.edu>
2018-03-12 12:27:01 -0700

    Updated, still have to change names of tab_2's tiff's

--- FireApp/Justin_Update/app.R
+++ FireApp/Justin_Update/app.R
@@ -66,6 +66,33 @@ RegimeNames <- unique(fireregime$FireRegime)
 palfireregime <- colorFactor(palette = c("burlywood4", "yellow","orange","red", "darkgrey", "deepskyblue"), domain = RegimeNames)
 
 
+Subset_2000 <- read.csv("Subset_2000.csv")
+
+SDI260_CFL_Change <- Subset_2000 %>% 
+  mutate(CFL_Change = Average_260 - Average_nt) %>% 
+  select(CFL_Change)
+
+
+choice <- c("standras", "aspectras30", "SlopTeal30d")
+
+standras <- raster("standras.tif")
+
+aspectras30 <- raster("aspectras30.tif")
+
+tiff_stack <- raster::stack("standras.tif", "aspectras30.tif", "SlopTeal30d.tif")
+
+private <- st_read(dsn = ".", layer = "Private_Parcels")
+
+private_t <- st_transform(private, "+init=epsg:4326")
+
+private_tclass <- private_t %>% 
+  select(Stand_Id)
+
+
+
+pal <- colorNumeric(c("#0C2C84", "#41B6C4", "#FFFFCC"), values(tiff_stack),
+                    na.color = "transparent")
+
 # Adapting this for my data
 
 #requires: SDI260_CFL_Change from SavingSierras_Stats.Rmd (which requires Subset_2000.csv)
@@ -82,6 +109,8 @@ ui <- dashboardPage(
     
     sidebarMenu(
       
+      menuItem("Fire Severity Histogram", tabName = "tab_1"),
+      menuItem("Static Maps", tabName = "tab_2"),
       menuItem("Fire History", tabName = "tab_3"),
       menuItem("Forest Cover", tabName = "tab_4")
   
@@ -92,6 +121,35 @@ ui <- dashboardPage(
   
   dashboardBody(
     tabItems(
+      
+      tabItem(tabName = "tab_1",
+              fluidRow(
+                box(plotOutput("my_graph1", height = 342)), 
+                box(title = "Choose Bins:", 
+                    sliderInput("bins",
+                                "Number of bins:",
+                                min = 1,
+                                max = 100,
+                                value = 30),
+                    selectInput(inputId = "bincolor",
+                                label = "Color",
+                                choices = colors(),
+                                selected = "firebrick"),
+                    
+                    checkboxInput(inputId = "addmean",
+                                  label = "Add Mean Line?",
+                                  value = FALSE)),
+                tabBox(tabPanel("Summary", verbatimTextOutput("summary")))
+              )),
+      
+      tabItem(tabName = "tab_2",
+              fluidRow(
+                box(leafletOutput("my_graph2", height = 432)),
+                box(title = "Dinkey Landscape Maps",
+                    selectInput("class", 
+                                "Choose Map:", 
+                                choices = choice))
+              )),
 
       tabItem(tabName = "tab_3",
               fluidRow(
@@ -117,6 +175,52 @@ ui <- dashboardPage(
   
 server <- function(input, output){
   
+  
+  output$my_graph1 <- renderPlot({
+    # generate bins based on input$bins from ui.R
+    x    <- SDI260_CFL_Change$CFL_Change 
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    output$summary <- renderPrint({
+      x <- SDI260_CFL_Change$CFL_Change             # Define x again
+      summary(x, digits = 3)
+    })
+    
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, border = 'white', col = input$bincolor, main = "Change in Fire Severity After Treatment", xlab = "Change in Conditional Flame Length (ft)")
+    if(input$addmean) {
+      
+      abline(v = mean(x),
+             lwd = 2,
+             lty = 2)
+      
+    }
+    
+  })
+  
+  
+  
+  
+  output$my_graph2 <- renderLeaflet({
+    
+    tiffmap <- subset(tiff_stack, input$class, drop=TRUE)
+    
+    #leaflet(private_tclass) %>% 
+    #addTiles() %>% 
+    
+    
+    leaflet(private_tclass) %>% 
+      addTiles() %>% 
+      addRasterImage(tiffmap, colors = pal, opacity = 0.8) %>% 
+      addLegend (pal = pal, values = values(tiffmap),
+                 title = input$class) %>% 
+      addPolygons(color = "black",
+                  weight = 0.5, fill = NA)
+    
+    
+    
+    
+  })
   
   output$my_graph3 <- renderLeaflet({
     regime_sub <- regime_class %>%
